dcbdee65d652abab95ad17e1c8627a6a8cddaec5
qingzma <qingzma@gmail.com>
2016-10-27 20:16:07 +0100

    this can work well

--- src/main/java/com/teamProject/App.java
+++ src/main/java/com/teamProject/App.java
@@ -9,6 +9,7 @@ package com.teamProject;
 
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -19,6 +20,8 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import org.jzy3d.chart.AWTChart;
+import org.jzy3d.chart.Chart;
+import org.jzy3d.chart.factories.IChartComponentFactory;
 import org.jzy3d.colors.Color;
 import org.jzy3d.colors.ColorMapper;
 import org.jzy3d.colors.colormaps.ColorMapRainbow;
@@ -51,104 +54,48 @@ public class App extends Application {
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
 
-        //Button  btnRead=new Button("Read File");
-        //btnRead.setOnAction((ActionEvent e)->{
         
-        
-        
-        /*
-        Cluster[] c=km.getClusters();
-        for(int i=0;i<km.getClusters().length;i++){
-            rm=new RegressionModel(c[i].getCluster().getX().toArray(),
-                                   c[i].getCluster().getY().toVector());
-            if(c[i].getCluster().getX().getDimension()==1)
-                rm.polynominal1D(4);
-            else if(c[i].getCluster().getX().getDimension()>=1)
-                rm.polynominalMD2P();
-            else{
-                System.out.println("error detected!");
-                System.exit(1);
-            }
-            
-            System.out.println("R2 of cluster "+i+" is "+rm.getR2());
-            //System.out.println("Beta "+i+" is "+rm.getBeta());
-        }
-        
-        c[0].getRange();
-           
-        */
-            /*
-            double[][] X=dc.getX().toArray();
-            double[] Y=dc.getY().toVector();
-            rm=new RegressionModel(X, Y);
-            rm.polynominal1D(5);
-            */
-            
-            /*
-            Point p1=new Point();
-            p1.add(1);
-            p1.add(3);
-            Point p2=new Point();
-            p2.add(2);
-            p2.add(6);
-            Point p3=new Point();
-            p3.add(3);
-            p3.add(9);
-            Points pts=new Points();
-            pts.add(p1);
-            pts.add(p2);
-            pts.add(p3);
-            
-            rm=new RegressionModel(pts.getX().toArray(), pts.getY().toVector());
-            rm.polynominal1D(0);
-            */
-            
-            
-            
-            
-             /*
-            Point p1=new Point();
-            p1.add(1);
-            p1.add(0.5);
-            p1.add(0.25);
-            p1.add(3);
-            Point p2=new Point();
-            p2.add(1);
-            p2.add(1);
-            p2.add(1);
-            p2.add(7);
-            Point p3=new Point();
-            p3.add(1);
-            p3.add(1);
-            p3.add(2);
-            p3.add(11);
-            Point p4=new Point();
-            p4.add(2);
-            p4.add(1);
-            p4.add(1);
-            p4.add(8);
-            Points pts=new Points();
-            pts.add(p1);
-            pts.add(p2);
-            pts.add(p3);
-            pts.add(p4);
-            
-            rm=new RegressionModel(pts.getX().toArray(), pts.getY().toVector());
-            rm.polynominalMD1P();
-            System.out.println("R2 is "+rm.getR2());
-            System.out.println("Beta "+" is "+rm.getBeta());
-            */
-           
-       // });
-        
-        
-        
-        
+        Button btn=new Button("FX");
+        btn.setOnAction((ActionEvent)->{
+            showFX();
+    });
 
         //root.add(btnRead,0,5);
-        root.add(dimensionBox(),0,4);
+        root.add(dimensionBox(),0,0);
+        root.add(btn, 0, 1);
         //root.add(wekaPane(),0,7);
         //root.add(orsonChart(),0,2);
+        
+        
+        
+        // Jzy3d
+        JavaFXChartFactory factory = new JavaFXChartFactory();
+
+        AWTChart chart = (AWTChart) factory.newChart(Quality.Advanced, IChartComponentFactory.Toolkit.offscreen);
+        
+        // Define a function to plot
+        Mapper mapper = new Mapper() {
+            @Override
+            public double f(double x, double y) {
+                return x * Math.sin(x * y);
+            }
+        };
+                // Define range and precision for the function to plot
+        Range range = new Range(-3, 3);
+        int steps = 80;
+        
+        // Create the object to represent the function over the given range.
+        final Shape surface = Builder.buildOrthonormal(mapper, range, steps);
+        surface.setColorMapper(new ColorMapper(new ColorMapRainbow(), surface.getBounds().getZmin(), surface.getBounds().getZmax(), new Color(1, 1, 1, .5f)));
+        surface.setFaceDisplayed(true);
+        surface.setWireframeDisplayed(false);
+        
+        chart.add(surface);
+        ImageView imageView=factory.bindImageView(chart);
+        
+        root.add(imageView, 0, 5);
+        factory.addSceneSizeChangedListener(chart, scene);
+        
         
         primaryStage.show();
     }
@@ -198,49 +145,44 @@ public class App extends Application {
         hbDimension.getChildren().addAll(lblDimension,tfWidth,btnDimension);
         return hbDimension;
     }
-/*
-     public void runFX() {
-         Stage stage=new Stage();
-        stage.setTitle(DemoJzy3dFX.class.getSimpleName());
+
+    private void showFX(){
+        Stage newStage=new Stage();
+        newStage.setTitle("FX Demo");
+        newStage.setHeight(600);
+        newStage.setWidth(1000);
         
-        // Jzy3d
+         // Jzy3d
         JavaFXChartFactory factory = new JavaFXChartFactory();
         AWTChart chart  = getDemoChart(factory, "offscreen");
         ImageView imageView = factory.bindImageView(chart);
-
-        // JavaFX
-        StackPane pane = new StackPane();
-        Scene scene = new Scene(pane);
-        stage.setScene(scene);
-        stage.show();
-        pane.getChildren().add(imageView);
-
-        factory.addSceneSizeChangedListener(chart, scene);
         
-        stage.setWidth(500);
-        stage.setHeight(500);
+        GridPane pane=new GridPane();
+        Scene scene=new Scene(pane);
+        pane.add(imageView, 0, 0);
+        
+        newStage.setScene(scene);
+        newStage.show();
     }
 
     private AWTChart getDemoChart(JavaFXChartFactory factory, String toolkit) {
-        // -------------------------------
-        // Define a function to plot
+      // Define a function to plot
         Mapper mapper = new Mapper() {
             @Override
             public double f(double x, double y) {
                 return x * Math.sin(x * y);
             }
         };
-
-        // Define range and precision for the function to plot
+                // Define range and precision for the function to plot
         Range range = new Range(-3, 3);
         int steps = 80;
-
+        
         // Create the object to represent the function over the given range.
         final Shape surface = Builder.buildOrthonormal(mapper, range, steps);
         surface.setColorMapper(new ColorMapper(new ColorMapRainbow(), surface.getBounds().getZmin(), surface.getBounds().getZmax(), new Color(1, 1, 1, .5f)));
         surface.setFaceDisplayed(true);
         surface.setWireframeDisplayed(false);
-
+        
         // -------------------------------
         // Create a chart
         Quality quality = Quality.Advanced;
@@ -248,9 +190,10 @@ public class App extends Application {
         //quality.setAnimated(true);
         
         // let factory bind mouse and keyboard controllers to JavaFX node
-        AWTChart chart = (AWTChart) factory.newChart(quality, toolkit);
+        AWTChart chart =  (AWTChart) factory.newChart(quality, toolkit);
         chart.getScene().getGraph().add(surface);
         return chart;
-    }
-  */
+
+      
+  }
 }
