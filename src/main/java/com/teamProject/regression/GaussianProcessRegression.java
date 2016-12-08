de0e73b932d9500cd93a5b07c2385c423f5cbc4a
wangxieric <wangxieric@gmail.com>
2016-12-08 16:54:30 +0000

    from wang

--- src/main/java/com/teamProject/regression/GaussianProcessRegression.java
+++ src/main/java/com/teamProject/regression/GaussianProcessRegression.java
@@ -8,6 +8,7 @@ package com.teamProject.regression;
 import com.teamProject.cluster.Cluster;
 import com.teamProject.cluster.KMeans;
 import com.teamProject.data.Points;
+import static java.lang.Math.*;
 import weka.classifiers.functions.GaussianProcesses;
 import weka.core.Attribute;
 import weka.core.DenseInstance;
@@ -25,7 +26,7 @@ public class GaussianProcessRegression implements RegressionInterface{
     private double timeCost;
     public Points points;
     private KMeans km;
-    private int clusterNum = 5;
+    private int clusterNum = 10;
     private GaussianProcesses[] gp = new GaussianProcesses[clusterNum];
     private Cluster[] clusters;
     
@@ -49,6 +50,7 @@ public class GaussianProcessRegression implements RegressionInterface{
     
     @Override
     public void run() {
+        System.out.println("run");
         Runtime runtime = Runtime.getRuntime();
         long start = System.currentTimeMillis();
         km = new KMeans(points,clusterNum);
@@ -99,7 +101,8 @@ public class GaussianProcessRegression implements RegressionInterface{
                 gp[i] = new GaussianProcesses();
                 gp[i].buildClassifier(data);
                 //System.out.println(data.toString());
-                
+                //RMSE();
+                //NRMSE();
             }
         }catch(Exception e){
             e.printStackTrace();
@@ -107,6 +110,7 @@ public class GaussianProcessRegression implements RegressionInterface{
         long end = System.currentTimeMillis();
         timeCost = end - start;
         memoryCost = runtime.totalMemory() - runtime.freeMemory();
+        
     }
     
     @Override
@@ -153,12 +157,106 @@ public class GaussianProcessRegression implements RegressionInterface{
     
     @Override
     public double NRMSE() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int N = 0;
+        int[] ni = new int[clusterNum];
+        double[] nrmse_i = new double[clusterNum];
+        double nrmse = 0;
+        double yAverage = 0.0;
+        try{ 
+            for(int i = 0;i<clusterNum;i++){
+                double[][] trainData = clusters[i].toArray();
+                for(int k = 0;k < trainData.length;k++){
+                    yAverage = yAverage + trainData[k][trainData[k].length-1];
+                }
+                N += trainData.length;
+            }
+            yAverage = yAverage / N;
+            System.out.println("yAverage: "+ yAverage);
+            for(int i = 0;i<clusterNum;i++){
+                nrmse_i[i] = 0.0;
+                double[][] trainData = clusters[i].toArray();
+                FastVector fv = new FastVector(trainData[0].length-1);
+                Instances data = new Instances("trainData",fv,trainData.length);
+                for(int k = 0;k<trainData[0].length-1;k++){
+                    StringBuilder sb = new StringBuilder();
+                    sb.append("x");
+                    sb.append(k);
+                    String s = sb.toString();
+                    Attribute attr = new Attribute(s);
+                    data.insertAttributeAt(attr, k);
+                }
+                Attribute attr = new Attribute("y");
+                data.insertAttributeAt(attr, trainData[0].length-1);
+                for(int k = 0;k<trainData.length;k++){
+                    Instance inst = new DenseInstance(trainData[k].length);
+                    for(int x = 0;x<trainData[k].length;x++){
+                        inst.setValue(x, trainData[k][x]);
+                    }
+                    data.add(inst);
+                    double[][] result = gp[i].predictIntervals(data.get(k), 0.95);
+                    //System.out.println("result: " + result[0][0] + " " + result[0][1]);
+                    nrmse_i[i] += (pow(((result[0][0]+result[0][1])/2 - trainData[k][trainData[k].length-1]),2)
+                            /pow((trainData[k][trainData[k].length-1]-yAverage),2)); 
+                }
+                nrmse_i[i] = sqrt(nrmse_i[i]/trainData.length);
+                System.out.println("nrmse_i: " + nrmse_i[i]);
+                ni[i] = trainData.length;
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        for(int i = 0;i<clusterNum;i++){
+            nrmse += ni[i]*pow(nrmse_i[i],2);
+        }
+        System.out.println("NRMSE: " + sqrt(nrmse/N));
+        return sqrt(nrmse/N);
     }
     
     @Override
     public double RMSE() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int N = 0;
+        int[] ni = new int[clusterNum];
+        double[] rmse_i = new double[clusterNum];
+        double rmse = 0;
+       
+        try{ 
+            for(int i = 0;i< clusterNum;i++){
+                rmse_i[i] = 0.0;
+                double[][] trainData = clusters[i].toArray();
+                FastVector fv = new FastVector(trainData[0].length-1);
+                Instances data = new Instances("trainData",fv,trainData.length);
+                for(int k = 0;k<trainData[0].length-1;k++){
+                    StringBuilder sb = new StringBuilder();
+                    sb.append("x");
+                    sb.append(k);
+                    String s = sb.toString();
+                    Attribute attr = new Attribute(s);
+                    data.insertAttributeAt(attr, k);
+                }
+                Attribute attr = new Attribute("y");
+                data.insertAttributeAt(attr, trainData[0].length-1);
+                for(int k = 0;k<trainData.length;k++){
+                    Instance inst = new DenseInstance(trainData[k].length);
+                    for(int x = 0;x<trainData[k].length;x++){
+                        inst.setValue(x, trainData[k][x]);
+                    }
+                    data.add(inst);
+                    double[][] result = gp[i].predictIntervals(data.get(k), 0.95);
+                    //System.out.println("result: " + result[0][0] + " " + result[0][1]);
+                    rmse_i[i]+=pow(((result[0][0]+result[0][1])/2 - trainData[k][trainData[k].length-1]),2); 
+                }
+                rmse_i[i] = sqrt(rmse_i[i]/trainData.length);
+                ni[i] = trainData.length;
+                N += trainData.length;
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        for(int i = 0;i<clusterNum;i++){
+            rmse += ni[i]*pow(rmse_i[i],2);
+        }
+        System.out.println("RMSE: " + sqrt(rmse/N));
+        return sqrt(rmse/N);
     }
     
     @Override
